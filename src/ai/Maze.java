65a7322ca4b332d322bd7c7714334d08194c5c23
Matthew <matbulat@gmail.com>
2015-12-01 00:21:03 +0000

    First Commit

---
+++ src/ai/Maze.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai;
+
+/**
+ *
+ * @author Matthew
+ */
+public class Maze {
+    private int left;
+    private int right;
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+    
+    
+}
